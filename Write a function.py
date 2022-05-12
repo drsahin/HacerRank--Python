5d922f9bff3f5a839de1290f3c5af6f86929c0a8
drsahin <drsahin5555@gmail.com>
2022-05-12 09:27:47 +0300

    task added

--- Write a function.py
+++ Write a function.py
@@ -33,7 +33,7 @@ def is_leap(year):
         leap = True
     elif year%4 == 0 and year%100 != 0:
         leap = True
-    
     return leap
-    
-    
+
+year = int(input())
+print(is_leap(year))
